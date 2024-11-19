de887c895a114d1e6b6a423f6343c2c7d6c73cab
HelloJC ✌ <167858059+HelloJC24@users.noreply.github.com>
2024-11-19 12:12:20 +0800

    Updates

--- firebase-config.js
+++ firebase-config.js
@@ -32,6 +32,10 @@ navigator.serviceWorker.register('https://kiefers-app.github.io/keepers_app/fire
     document.getElementById("hg").remove();
   }
 
+  const currentUrl = window.location.href;
+  if(window.location.href == 'https://kiefers-app.github.io/keepers_app/#/tracker/' || window.location.href == 'https://kiefers-app.github.io/keepers_app/#/setlocation/'){
+    document.getElementById("hg").remove();
+  }
 
   function hideper () {
     document.getElementById("hg").remove();
